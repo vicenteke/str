30d891f116a6af89328a021e7935cf887d1bbc76
Vicente Knihs Erbs <vicenteknihs@hotmail.com>
2021-03-05 16:37:05 -0300

    Parece que tudo funciona

--- 2_Trabalho/main.cc
+++ 2_Trabalho/main.cc
@@ -1,7 +1,9 @@
 #include "timer.h"
 #include "task.h"
+#include <sched.h>
+#include <cstring>
 
-#define REPETITIONS 15 // Number of repetitions to stop program
+#define REPETITIONS 25 // Number of repetitions to stop program
 
 int main(int argc, char const *argv[])
 {
@@ -14,6 +16,12 @@ int main(int argc, char const *argv[])
     if (argc > 4) {
         period = (unsigned int)atoi(argv[1]);
         priority = atoi(argv[2]);
+
+        if (priority < 1 || priority > 99) {
+            std::cout << "Priority should be between 1 - 99, set to 5...\n";
+            priority = 5;
+        }
+
         load = (unsigned long)atoi(argv[3]);
         isFIFO = (argv[4][6] == 'F');
     } else {
@@ -21,6 +29,22 @@ int main(int argc, char const *argv[])
         return 0;
     }
     std::cout << "\nSettings:\n    Period: " << period << "\n    Priority: " << priority << "\n    Load: " << load << "\n    Scheduler: " << argv[4] << "\n\n";
+
+    // Set scheduler
+    int policy = (isFIFO) ? SCHED_FIFO : SCHED_RR;
+    sched_param param;
+    sched_getparam(0, &param);
+    param.sched_priority = priority;
+
+    if(sched_setscheduler(0, policy, &param) == -1) {
+        std::cout << "Failed to set scheduler options, running on default...\n" << std::strerror(errno) << std::endl;
+    }
+    // } else {
+    //     if(!isFIFO) {
+    //         std::cout << "    RR Interval: " << sched_rr_get_interval() << '\n';
+    //     }
+    //     std::cout << '\n';
+    // }
 
     // Starts execution
     Task::_timer = new Timer(period);
